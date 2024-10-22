aa8682e5e572c0c14878b7fde697d8628f2b30e6
SivasankariSK <146175570+SivasankariSK@users.noreply.github.com>
2024-10-22 11:47:38 +0530

    Add files via upload

---
+++ work (1).js
@@ -0,0 +1,35 @@
+//1.print hello world using js
+console.log("hello world");
+// 2.declaring variables
+
+var a= 3;
+function declaration(){
+    var b=6;
+    let a = 7;
+    console.log(a,b);
+}
+declaration();
+console.log(a);
+//3.eligible for vote or not
+let age =
+if (age >= 18) {
+  console.log("You are eligible to vote!");
+} else {
+  console.log("You are not eligible to vote yet!");
+}
+//4.. Function to add two numbers:
+Let a=2;
+let b=4;
+function addNumbers(a, b) {
+  return a + b;
+}
+//5.avaScript program to calculate factorial using a for loop:
+
+let num = prompt("Enter a number:");
+let factorial = 1;
+for (let i = 1; i <= num; i++) {
+  factorial *= i;
+}
+
+
+
